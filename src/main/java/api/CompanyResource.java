be6eca6d754d6454979d3f3745c2eca6858a0e08
daviddk222 <danielwinkel4@hotmail.com>
2016-10-05 12:56:29 +0200

    Added getAll GET method in CompanyResource as api/company/complete

--- src/main/java/api/CompanyResource.java
+++ src/main/java/api/CompanyResource.java
@@ -1,10 +1,15 @@
 package api;
 
 import facade.CompanyFacade;
+import javax.ws.rs.GET;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 import jsonconverter.JSONConverter;
+import static org.glassfish.jersey.uri.UriComponent.Type.PATH;
 
 /**
  * REST Web Service
@@ -26,6 +31,11 @@ public class CompanyResource {
     private JSONConverter jCon = JSONConverter.getClassInstance();
     private CompanyFacade cFacade = CompanyFacade.getInstance();
     
-    
+    @GET
+    @Path("/complete")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAll() {
+        return Response.ok().entity(jCon.getJsonFromCompaniesAllInfo(cFacade.getCompanies())).build();
+    }
     
 }
