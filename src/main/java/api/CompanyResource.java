85db0e4d6495752517ff64250f76158e338a47cf
daviddk222 <danielwinkel4@hotmail.com>
2016-10-05 13:05:51 +0200

    Updated CompanyResource with exception handling

--- src/main/java/api/CompanyResource.java
+++ src/main/java/api/CompanyResource.java
@@ -36,14 +36,35 @@ public class CompanyResource {
     @GET
     @Path("/complete")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getAll() {
+    public Response getAllComplete() {
         return Response.ok().entity(jCon.getJsonFromCompaniesAllDetails(cFacade.getCompanies())).build();
     }
 
     @GET
     @Path("/complete/{cvr}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Response get(@PathParam("cvr") String cvr) throws CustomException {
+    public Response getComplete(@PathParam("cvr") String cvr) throws CustomException {
+
+        Company c = cFacade.getCompany(cvr);
+
+        if (c == null) {
+            throw new CustomException(404, "No company with that cvr was found");
+        }
+
+        return Response.ok().entity(jCon.getJsonFromCompanyAllDetails(c)).build();
+    }
+    
+    @GET
+    @Path("/contactinfo")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getAllContact() {
+        return Response.ok().entity(jCon.getJsonFromCompaniesContactInfo(cFacade.getCompanies())).build();
+    }
+
+    @GET
+    @Path("/contactinfo/{cvr}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getContact(@PathParam("cvr") String cvr) throws CustomException {
 
         Company c = cFacade.getCompany(cvr);
 
