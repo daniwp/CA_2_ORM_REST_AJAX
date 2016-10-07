245df62bac91b11df27f679218d80be12b54989f
daviddk222 <danielwinkel4@hotmail.com>
2016-10-07 18:24:42 +0200

    added number formating on front-end

--- src/main/webapp/javascript/companyScript.js
+++ src/main/webapp/javascript/companyScript.js
@@ -17,8 +17,8 @@ $(document).ready(function () {
                     coutput += typeof company.email !== "undefined" ? '<td>' + company.email + '</td>' : '<td>-</td>';
                     coutput += typeof company.description !== "undefined" ? '<td>' + company.description + '</td>' : '<td>-</td>';
                     coutput += typeof company.cvr !== "undefined" ? '<td>' + company.cvr + '</td>' : '<td>-</td>';
-                    coutput += typeof company.numEmployees !== "undefined" ? '<td>' + company.numEmployees + '</td>' : '<td>-</td>';
-                    coutput += typeof company.marketValue !== "undefined" ? '<td>' + company.marketValue + '</td>' : '<td>-</td>';
+                    coutput += typeof company.numEmployees !== "undefined" ? '<td>' + formatNumber2(company.numEmployees) + '</td>' : '<td>-</td>';
+                    coutput += typeof company.marketValue !== "undefined" ? '<td>' + formatNumber(company.marketValue) + '$' + '</td>' : '<td>-</td>';
                     $('#companybody').append('<tr>' + coutput + '</tr>');
                 });
             }, error: function (res) {
@@ -76,6 +76,32 @@ $(document).ready(function () {
             }
         });
     });
+
+    function formatNumber(number)
+    {
+        number = number.toFixed(2) + '';
+        x = number.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        }
+        return x1 + x2;
+    }
+
+    function formatNumber2(number)
+    {
+        number = number.toFixed(2) + '';
+        x = number.split('.');
+        x1 = x[0];
+        x2 = x.length > 1 ? '.' + x[1] : '';
+        var rgx = /(\d+)(\d{3})/;
+        while (rgx.test(x1)) {
+            x1 = x1.replace(rgx, '$1' + ',' + '$2');
+        }
+        return x1;
+    }
 
 });
 
