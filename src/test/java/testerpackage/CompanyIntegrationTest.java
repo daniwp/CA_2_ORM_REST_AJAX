9486318395f01f6881d63365f39eb8c6ec57f5a8
John <John@DESKTOP-34E2KUC.home>
2016-10-08 11:10:28 +0200

    more tests added, 12/12 passed

--- src/test/java/testerpackage/CompanyIntegrationTest.java
+++ src/test/java/testerpackage/CompanyIntegrationTest.java
@@ -1,106 +1,106 @@
-//package testerpackage;
-//
-//import static io.restassured.RestAssured.*;
-//import io.restassured.http.ContentType;
-//import static org.hamcrest.Matchers.*;
-//import org.junit.Test;
-//
-///**
-// *
-// * @author John
-// */
-//public class CompanyIntegrationTest {
-//    private String baseUrl = "http://localhost:8084/CA2REST/api/company";
-//    
-//    public CompanyIntegrationTest() {
-//        
-//    }
-//    
-//    @Test
-//    public void VerifyAllCompanies() {
-//        given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get(baseUrl + "/complete")
-//                .then()
-//                .statusCode(200)
-//                //Checking for dummy-data in several json objects
-//                .body("firstname", hasItems("Daniel", "Mohammed", "David"));
-//    }
-//    
-//    @Test
-//    public void VerifySingleCvr() {
-//        given()
-//                .contentType(ContentType.JSON)
-//                .pathParam("cvr","384729")
-//                .when()
-//                .get(baseUrl + "/complete/cvr/{cvr}")
-//                .then()
-//                .statusCode(200)
-//                .body("name", equalTo("DWP Web"));
-//    }
-//    
-//    @Test
-//    public void VerifySingleCvrException() {
-//        given()
-//                .contentType(ContentType.JSON)
-//                .pathParam("cvr","999999999")
-//                .when()
-//                .get(baseUrl + "/complete/cvr/{cvr}")
-//                .then()
-//                .statusCode(404)
-//                .body("message", equalTo("No company with that cvr was found"));
-//    }
-//    
-//    @Test
-//    public void UndefinedCompany() {
-//        given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get(baseUrl)
-//                .then()
-//                .statusCode(404)
-//                .body("message", equalTo("The service you requested does not exist."));
-//    }
-//    
-//    @Test
-//    public void VerifyPhone() {
-//        given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .pathParam("phone", "93949192")
-//                .get(baseUrl + "/complete/phone/{phone}")
-//                .then()
-//                .statusCode(200)
-//                .body("name", equalTo("DWP Web"));
-//    }
-//    
-//    @Test
-//    public void VerifyContactInfo() {
-//        given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get(baseUrl + "/contactinfo")
-//                .then()
-//                .statusCode(200)
-//                .body("id", hasItems(1,2,3));
-//    }
-//    
-//    @Test
-//    public void VerifyDeleteCompanyException() {
-//        given()
-//                .contentType(ContentType.JSON)
-//                .pathParam("id", "999")
-//                .when()
-//                .delete(baseUrl + "/{id}")
-//                .then()
-//                .statusCode(404)
-//                .body("message", equalTo("No company with that id was found"));
-//    }
-//    
-//    
-//    
-//    
-//    
-//}
-//
+package testerpackage;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.http.ContentType;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author John
+ */
+public class CompanyIntegrationTest {
+    private String baseUrl = "http://localhost:8084/CA2REST/api/company";
+    
+    public CompanyIntegrationTest() {
+        
+    }
+    
+    @Test
+    public void VerifyAllCompanies() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(baseUrl + "/complete")
+                .then()
+                .statusCode(200)
+                //Checking for dummy-data in several json objects
+                .body("name", hasItems("Mo's kebab", "DWP Web", "CES A/S"));
+    }
+    
+    @Test
+    public void VerifySingleCvr() {
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("cvr","384729")
+                .when()
+                .get(baseUrl + "/complete/cvr/{cvr}")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("DWP Web"));
+    }
+    
+    @Test
+    public void VerifySingleCvrException() {
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("cvr","999999999")
+                .when()
+                .get(baseUrl + "/complete/cvr/{cvr}")
+                .then()
+                .statusCode(404)
+                .body("message", equalTo("No company with that cvr was found"));
+    }
+    
+    @Test
+    public void UndefinedCompany() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(baseUrl)
+                .then()
+                .statusCode(404)
+                .body("message", equalTo("The service you requested does not exist."));
+    }
+    
+    @Test
+    public void VerifyPhone() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .pathParam("phone", "93949192")
+                .get(baseUrl + "/complete/phone/{phone}")
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("DWP Web"));
+    }
+    
+    @Test
+    public void VerifyContactInfo() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(baseUrl + "/contactinfo")
+                .then()
+                .statusCode(200)
+                .body("id", hasItems(1,2,3));
+    }
+    
+    @Test
+    public void VerifyDeleteCompanyException() {
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("id", "999")
+                .when()
+                .delete(baseUrl + "/{id}")
+                .then()
+                .statusCode(404)
+                .body("message", equalTo("No company with that id was found"));
+    }
+    
+    
+    
+    
+    
+}
+
--- src/test/java/testerpackage/PersonIntegrationTest.java
+++ src/test/java/testerpackage/PersonIntegrationTest.java
@@ -1,82 +1,82 @@
-//package testerpackage;
-//
-//import static io.restassured.RestAssured.*;
-//import io.restassured.http.ContentType;
-//import static org.hamcrest.Matchers.*;
-//import org.junit.Test;
-//
-///**
-// *
-// * @author John
-// */
-//public class PersonIntegrationTest {
-//
-//    private String baseUrl = "http://localhost:8084/CA2REST/api/person";
-//
-//    public PersonIntegrationTest() {
-//    }
-//
-//    @Test
-//    public void VerifyAllPersons() {
-//        given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get(baseUrl + "/complete")
-//                .then()
-//                .statusCode(200)
-//                //Checking for dummy-data in several json objects
-//                .body("array", hasItems("Daniel", "Mohammed", "David"));
-//
-//    }
-//
-//    @Test
-//    public void VerifyPersonById() {
-//        given()
-//                .contentType(ContentType.JSON)
-//                .pathParam("id", 1)
-//                .when()
-//                .get(baseUrl + "/complete/{id}")
-//                .then()
-//                .statusCode(200)
-//                .body("firstname", equalTo("Daniel"))
-//                .body("email", equalTo("dwp@dwp.com")); //email of person with id=1;
-//    }
-//
-//    @Test
-//    public void VerifyContacts() {
-//        given()
-//                .contentType(ContentType.JSON)
-//                .when()
-//                .get(baseUrl + "/contactinfo")
-//                .then()
-//                .statusCode(200)
-//                .body("id", hasItems(1, 2, 3));
-//    }
-//
-//    @Test
-//    public void VerifyContactById() {
-//        int id = 1;
-//        given()
-//                .contentType(ContentType.JSON)
-//                .pathParam("id", id)
-//                .when()
-//                .get(baseUrl + "/contactinfo/{id}")
-//                .then()
-//                .statusCode(200)
-//                .body("id", equalTo(id));
-//    }
-//
-//    @Test
-//    public void VerifyContactByIdException() {
-//        int id = 999;
-//        given()
-//                .contentType(ContentType.JSON)
-//                .pathParam("id", id)
-//                .when()
-//                .get(baseUrl + "/contactinfo/{id}")
-//                .then()
-//                .statusCode(404)
-//                .body("message", equalTo("No person with that id was found"));
-//    }
-//
-//}
+package testerpackage;
+
+import static io.restassured.RestAssured.*;
+import io.restassured.http.ContentType;
+import static org.hamcrest.Matchers.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author John
+ */
+public class PersonIntegrationTest {
+
+    private String baseUrl = "http://localhost:8084/CA2REST/api/person";
+
+    public PersonIntegrationTest() {
+    }
+
+    @Test
+    public void VerifyAllPersons() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(baseUrl + "/complete")
+                .then()
+                .statusCode(200)
+                //Checking for dummy-data in several json objects
+                .body("firstname", hasItems("Daniel", "Mohammed", "David"));
+
+    }
+
+    @Test
+    public void VerifyPersonById() {
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("id", 1)
+                .when()
+                .get(baseUrl + "/complete/{id}")
+                .then()
+                .statusCode(200)
+                .body("firstname", equalTo("David"))
+                .body("email", equalTo("dwp@dwp.com")); //email of person with id=1;
+    }
+
+    @Test
+    public void VerifyContacts() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(baseUrl + "/contactinfo")
+                .then()
+                .statusCode(200)
+                .body("id", hasItems(1, 2, 3));
+    }
+
+    @Test
+    public void VerifyContactById() {
+        int id = 1;
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("id", id)
+                .when()
+                .get(baseUrl + "/contactinfo/{id}")
+                .then()
+                .statusCode(200)
+                .body("id", equalTo(id));
+    }
+
+    @Test
+    public void VerifyContactByIdException() {
+        int id = 999;
+        given()
+                .contentType(ContentType.JSON)
+                .pathParam("id", id)
+                .when()
+                .get(baseUrl + "/contactinfo/{id}")
+                .then()
+                .statusCode(404)
+                .body("message", equalTo("No person with that id was found"));
+    }
+
+}
